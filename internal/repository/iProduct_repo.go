99d97688657f86eda6f614d627a5e361bebd123c
Mulyadi Kamsul <mulyadikamsul@gmail.com>
2024-09-13 15:26:21 +0700

    fix3

--- internal/repository/iProduct_repo.go
+++ internal/repository/iProduct_repo.go
@@ -4,14 +4,12 @@ import (
 	"context"
 )
 
-// Product represents the product entity
 type Product struct {
 	ID    string `bson:"_id,omitempty"`
 	Name  string `bson:"name"`
 	Stock int32  `bson:"stock"`
 }
 
-// ProductRepository is an interface that defines the methods for product operations
 type IProductRepo interface {
 	Create(ctx context.Context, product *Product) error
 	GetByID(ctx context.Context, id string) (*Product, error)
